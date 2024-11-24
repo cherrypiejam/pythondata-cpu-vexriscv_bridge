ba33c6c73ba34869027611664748cd85d008fa47
Gongqi Huang <gongqih@hotmail.com>
2024-11-24 17:21:16 -0500

    fix minor

--- generate.py
+++ generate.py
@@ -2,9 +2,4 @@
 
 from litex.soc.cores.cpu.vexriscv_bridge import VexRiscvBridge
 
-import pythondata_cpu_vexriscv_smp;
-import pythondata_cpu_vexriscv_bridge;
-
-GGGGDDD
-
 VexRiscvBridge.generate_default_configs()
