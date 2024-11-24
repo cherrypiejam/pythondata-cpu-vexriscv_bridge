b33376ebc5209bae51cda8f11773395db28fd4fc
Gongqi Huang <gongqih@hotmail.com>
2024-11-24 17:14:52 -0500

    pythondata for vexriscv bridge cluster

--- generate.py
+++ generate.py
@@ -2,4 +2,9 @@
 
 from litex.soc.cores.cpu.vexriscv_bridge import VexRiscvBridge
 
+import pythondata_cpu_vexriscv_smp;
+import pythondata_cpu_vexriscv_bridge;
+
+GGGGDDD
+
 VexRiscvBridge.generate_default_configs()
